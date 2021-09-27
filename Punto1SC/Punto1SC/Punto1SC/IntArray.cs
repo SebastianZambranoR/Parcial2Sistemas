6d396b88a12e10fd05d184fd7225df12258ea340
Sebastian Zambrano <50499778+SebastianZambranoR@users.noreply.github.com>
2021-09-26 21:44:54 -0500

    Carga aplicacion en c# del punto 1

---
+++ Punto1SC/Punto1SC/Punto1SC/IntArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Punto1SC
+{
+    class IntArray
+    {
+        int[] array;
+
+        public IntArray(int size)//stack
+        {
+            array = new int[size];//heap
+            for(int i = 0; i < size; i++)//stack
+            {
+                array[i] = i;
+            }
+        }
+
+        public void PrintArray()
+        {
+            for (int i = 0; i < array.Length; i++)//stack
+            {
+                Console.WriteLine("array[{0}]: {1}",array[i], i);
+            }
+        }
+    }
+}
---
+++ Punto1SC/Punto1SC/Punto1SC/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Punto1SC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntArray intArray = new IntArray(100);//heap
+            intArray.PrintArray();
+        }
+    }
+}
